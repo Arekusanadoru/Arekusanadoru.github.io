76714ce137acb64fe79dd547ab7a186a18075ae0
Arekusanadoru <48053175+Arekusanadoru@users.noreply.github.com>
2020-08-07 20:07:09 +0600

    Update action.php
    07 08 20

--- action.php
+++ action.php
@@ -1,7 +1,16 @@
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  echo $name."<br />".$email."<br />";
-}
+﻿<?php
+$to = "89048279944@mail.ru";; // Здесь нужно написать e-mail, куда будут приходить письма
+$from = $_POST['email']; // this is the sender's Email address
+$subject = "Форма отправки сообщений с сайта";
+$subject2 = "Copy of your form submission";
+$message = "Hello hell";
+
+$headers = "From:" . $from;
+$headers2 = "From:" . $to;
+
+mail($to,$subject,$message,$headers);
+// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+echo "Сообщение отправлено. Спасибо Вам, мы скоро свяжемся с Вами.";
+echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+
 ?>
